c9b3202e8c787fc7e3760761554450528c08f097
sreyngitdoeurm <sreyngit.doeurm@student.passerellesnumeriques.org>
2021-10-26 17:09:18 +0700

    create router

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+class UserController extends Controller
+{
+    //
+    public function register(Request $request){
+
+    }
+    public function login(Request $request){
+    }
+    public function logout(Request $request){
+    }
+}
